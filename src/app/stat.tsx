3df497dd1ea9c801c0559ace8ff9c91be031432b
Fahri Setiawan <fahrisetiawan550@gmail.com>
2026-05-28 14:39:57 +0700

    layout 2

--- src/app/stat.tsx
+++ src/app/stat.tsx
@@ -1,5 +1,4 @@
-import { Badge } from '@/components/badge'
-import { Divider } from '@/components/divider'
+
 
 export function Stat({
 	title,
@@ -12,13 +11,13 @@ export function Stat({
 }) {
 	return (
 		<div>
-			<Divider />
+			{/* <Divider />
 			<div className="mt-6 text-lg/6 font-medium sm:text-sm/6">{title}</div>
 			<div className="mt-3 text-3xl/8 font-semibold sm:text-2xl/8">{value}</div>
 			<div className="mt-3 text-sm/6 sm:text-xs/6">
 				<Badge color={change.startsWith('+') ? 'lime' : 'pink'}>{change}</Badge>{' '}
 				<span className="text-zinc-500">from last week</span>
-			</div>
+			</div> */}
 		</div>
 	)
 }
