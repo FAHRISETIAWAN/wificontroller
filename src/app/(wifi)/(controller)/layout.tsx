09e2c4b45882d6b9741b3ec55a77d2e63fd4d054
Fahri Setiawan <fahrisetiawan550@gmail.com>
2026-05-28 14:34:40 +0700

    layout 1

--- src/app/(wifi)/(controller)/layout.tsx
+++ src/app/(wifi)/(controller)/layout.tsx
@@ -1,9 +1,8 @@
 import { ApplicationLayout } from '@/app/(wifi)/application-layout'
-import Header from '@/components/header/header'
 
 export default async function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <ApplicationLayout header={<Header variant="bg-transparent-text-white" hasBottomBorder={false} />}>
+    <ApplicationLayout>
       {children}
     </ApplicationLayout>
   )
